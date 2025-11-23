5dbdedd9f202402519893a80a1775fae6ee9f6b5
Varivonchik Artem <varivonchik.core@gmail.com>
2025-11-24 01:03:15 +0300

    chore(config): enable absolute import paths

--- configuration/build/buildResolvers.ts
+++ configuration/build/buildResolvers.ts
@@ -1,7 +1,22 @@
+import path from "path";
 import webpack from "webpack";
+import { BuildOptions } from "../types/configurationTypes";
 
-export function buildResolvers(): webpack.ResolveOptions {
+export function buildResolvers(options: BuildOptions): webpack.ResolveOptions {
+  const { paths } = options;
+
   return {
     extensions: [".tsx", ".ts", ".js"],
+    preferAbsolute: true,
+    modules: [paths.src, "node_modules"],
+    alias: {
+      "@": paths.src,
+      "@shared": path.resolve(paths.src, "shared"),
+      "@entities": path.resolve(paths.src, "entities"),
+      "@widgets": path.resolve(paths.src, "widgets"),
+      "@features": path.resolve(paths.src, "features"),
+      "@app": path.resolve(paths.src, "app"),
+    },
+    mainFiles: ["index"],
   };
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <div>1s</div>
+    <div>1</div>
   </React.StrictMode>
 );
