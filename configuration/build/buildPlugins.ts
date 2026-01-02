8b709ccf0f493ee26a5083d635f8afdf35013c20
varivonchik-artem <varivonchik.core@gmail.com>
2026-01-03 01:04:00 +0300

    feat: integrate global.d.ts file

--- configuration/build/buildPlugins.ts
+++ configuration/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "../types/configurationTypes";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
   const { paths } = options;
@@ -10,5 +11,9 @@ export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstan
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8]/.css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    })
   ];
 }
